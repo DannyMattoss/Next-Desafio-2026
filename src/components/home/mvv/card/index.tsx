a0ddffd9e62fba6fa39433e14810fd2546e5ffb2
Danny <dayanne.mattos@estudante.ufjf.br>
2026-06-04 21:26:33 -0300

    [fix]: corrige estilização mvv

--- src/components/home/mvv/card/index.tsx
+++ src/components/home/mvv/card/index.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 export default function Card() {
 
     return (
-            <div className="w-3/10 flex flex-col gap-5 bg-branco px-4 items-center py-8 rounded-2xl border-1 border-rosa  bg-preto/80">
+            <div className="w- flex flex-col gap-5 bg-mescuro px-4 items-center py-8 rounded-2xl shadow-[4px_4px_4px_rgba(166,126,126,1)]">
                 <div className="">
-                    <p className="text-3xl"> 
+                    <p className="text-3xl font-titulo text-rosasujo"> 
                         Missão
                     </p>
                 </div>
                 <div id="descmis" className="">
-                    <p className="text-2xl">
+                    <p className="text-sm font-texto text-rosasujo font-bold" >
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
