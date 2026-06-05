618585b60e65102add630647b82afee466707037
Danny <dayanne.mattos@estudante.ufjf.br>
2026-06-05 19:51:51 -0300

    [fix]: torna card dinamico

--- src/components/home/carrossel/cards/index.tsx
+++ src/components/home/carrossel/cards/index.tsx
@@ -2,25 +2,31 @@
 
 import Image from "next/image";
 import Link from "next/link";
+type CardProps = {
+  imagem:string;
+  titulo:string;
+  descricao:string;
+  preco:string;  
+}
 
-export default function Card() {
+export default function Card({imagem, titulo, descricao, preco}: CardProps) {
   return (
     <div className="flex flex-col bg-rosasujo rounded-2xl p-2.5 shadow-[4px_4px_4px_rgba(135,98,100,1)]">
       <Image
         width={1000}
         height={1000}
         alt=""
-        src="/assets/ampulheta.jpg"
+        src={imagem}
         className="rounded-2xl  w-full h-48"/>
       <div className="flex flex-col gap-3 py-2">
         <h1 className="font-subtitulo font-bold text-mescuro text-lg">
-            BlackPink - Deadline
+            {titulo}
         </h1>
         <p className="font-texto font-semibold text-mescuro text-sm">
-            Versão MOOD LIGHT do 3º MINI ÁLBUM [DEADLINE] do BLACKPINK
+            {descricao}
             </p>
         <p className="font-texto font-bold text-mescuro text-2xl text-center">
-            R$381,90
+            {preco}
         </p>
       </div>
     </div>
