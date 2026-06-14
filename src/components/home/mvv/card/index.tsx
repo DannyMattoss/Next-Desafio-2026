1ad5a828bcf7f612ddc39fda38d95f49fa1fbe5a
Danny <dayanne.mattos@estudante.ufjf.br>
2026-06-14 12:59:40 -0300

    [feat] implementa api do MVV

--- src/components/home/mvv/card/index.tsx
+++ src/components/home/mvv/card/index.tsx
@@ -2,18 +2,24 @@
 
 import Image from "next/image";
 import Link from "next/link";
-export default function Card() {
+
+interface CardProps {
+    title: string;
+    text: string;
+}
+
+export default function Card({title, text}: CardProps) {
 
     return (
             <div className="w- flex flex-col gap-5 bg-mescuro px-4 items-center py-8 rounded-2xl shadow-[4px_4px_4px_rgba(166,126,126,1)]">
                 <div className="">
                     <p className="text-3xl font-titulo text-rosasujo"> 
-                        Missão
+                        {title}
                     </p>
                 </div>
-                <div id="descmis" className="">
-                    <p className="text-sm font-texto text-rosasujo font-bold" >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <div className="">
+                    <p className="text-sm font-texto text-rosasujo font-bold">
+                        {text}
                     </p>
                 </div>
             </div>
--- src/components/home/mvv/index.tsx
+++ src/components/home/mvv/index.tsx
@@ -1,17 +1,34 @@
-'use client';
 
 import Image from "next/image";
 import Link from "next/link";
 import Card from "./card";
-export default function MVV() {
+import { getIdentities } from "@/src/lib/api/getmvv";
+export default async function MVV() {
+    try{
+        const data = await getIdentities();
+        return (
+            <ul className="flex flex-row w-full gap-4 px-44 py-16 items-center justify-between">
+                
+                {data.map((item, index) => (
+                    <li className="contents" key={item.id}>
+                        <Card 
+                            title={item.title} 
+                            text={item.text} 
+                        />
 
-    return (
-        <section className="flex flex-row w-full gap-4 px-44 py-16 items-center justify-between">
-        <Card/>
-        <i className="text-6xl text-mescuro bi bi-music-note"></i>
-        <Card/>
-        <i className="text-6xl text-mescuro bi bi-music-note"></i>
-        <Card/>
-        </section> 
-    );
+                        {index < data.length - 1 && (
+                            <i className="text-6xl text-mescuro bi bi-music-note"></i>
+                        )}
+                    </li>
+                ))}
+
+            </ul>
+        );
+           
+    } catch(error) {
+        console.error("Error fetching indentities", error);
+        return <p>Ocorreu um erro ao carregar os dados</p>
+
+    }
+    
 }
